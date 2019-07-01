058b4a643b1b766a7ce48f0b5ebdfdaae7fd8921
deyvsonaguiar <teen@0101>
2019-07-01 01:15:19 -0400

    Create Post and implemment tinyMCE

--- app/Http/Controllers/admin/PostController.php
+++ app/Http/Controllers/admin/PostController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers\admin;
 
+use App\Category;
 use App\Post;
+use App\Tag;
+use Brian2694\Toastr\Facades\Toastr;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class PostController extends Controller
 {
@@ -17,12 +24,58 @@ class PostController extends Controller
 
     public function create()
     {
-        return view('admin.post.create');
+        $categories = Category::all();
+        $tags = Tag::all();
+        return view('admin.post.create', compact('categories', 'tags'));
     }
 
     public function store(Request $request)
     {
-        //
+        $this->validate($request,[
+            'title' => 'required',
+            'image' => 'required',
+            'categories' => 'required',
+            'tags' => 'required',
+            'body' => 'required',
+        ]);
+        $image = $request->file('image');
+        $slug = str_slug($request->title);
+
+        if (isset($image)) {
+            //make unique name for image
+            $currentDate = Carbon::now()->toDateString();
+            $imageName = $slug.'-'.$currentDate.'-'.uniqid().'.'.$image->getClientOriginalExtension();
+            //check category dir is exists
+            if(!Storage::disk('public')->exists('post')){
+                Storage::disk('public')->makeDirectory('post');
+            }
+            //resize image for category and upload
+            $postImage = Image::make($image)->resize(1600,1066)->stream();
+            Storage::disk('public')->put('post/'.$imageName,$postImage);
+
+        } else {
+            $imageName = "default.png";
+        }
+
+        $post = new Post();
+        $post->user_id = Auth::id();
+        $post->title = $request->title;
+        $post->slug = $slug;
+        $post->image = $imageName;
+        $post->body = $request->body;
+        if(isset($request->status))
+        {
+            $post->status = true;
+        }else {
+            $post->status = false;
+        }
+        $post->is_approved = true;
+        $post->save();
+        $post->categories()->attach($request->categories);
+        $post->tags()->attach($request->tags);
+
+        Toastr::success('Postagem salva com sucesso :)','Sucesso!');
+        return redirect()->route('admin.posts.index');
     }
 
     public function show($id)
--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -4,6 +4,9 @@
 
 @push('css')
 
+    <!-- Bootstrap Select Css -->
+    <link href="{{ asset('assets/backend/plugins/bootstrap-select/css/bootstrap-select.css') }}" rel="stylesheet" />
+
 @endpush
 
 @section('content')
@@ -14,8 +17,11 @@
         </div>
 
         <!-- Vertical Layout -->
+    <form action="{{ route('admin.posts.store') }}" method="POST"
+          enctype="multipart/form-data">
+        @csrf
         <div class="row clearfix">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
                     <div class="card">
                         <div class="header">
                             <h2>
@@ -23,34 +29,70 @@
                             </h2>
                         </div>
                         <div class="body">
-                            <form action="{{ route('admin.posts.store') }}" method="POST"
-                            enctype="multipart/form-data">
-                                @csrf
-                                <label>Título</label>
-                                <div class="form-group">
-                                    <div class="form-line">
-                                        <input type="text" id="title" name="title" class="form-control" placeholder="Digite um título...">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="form-line">
-                                        <input type="file" id="image" name="image" class="form-control">
-                                    </div>
-                                </div>
-                                <label>Texto</label>
-                                <div class="form-group">
-                                    <div class="form-line">
-                                        <input type="text" id="body" name="body" class="form-control">
-                                    </div>
-                                </div>
-                                <br>
-                                <a href="{{ route('admin.category.index')}}" class="btn btn-danger m-t-15 waves-effect">VOLTAR</a>
-                                <button type="submit" class="btn btn-primary m-t-15 waves-effect">ENVIAR</button>
-                            </form>
+                            <div class="form-group">
+                                <label>Título:</label>
+                                <input type="text" id="title" name="title" class="form-control" placeholder="Digite um título...">
+                            </div>
+                            <div class="form-group">
+                                <label>Imagem:</label>
+                                <input type="file" id="image" name="image" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <input type="checkbox" id="publish" name="status" class="form-control" value="1">
+                                <label for="publish">Publicar</label>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+                <div class="card">
+                    <div class="header">
+                        <h2>
+                            CATEGORIAS E TAGS
+                        </h2>
+                    </div>
+                    <div class="body">
+                            <div class="form-group">
+                                {{ $errors->has('categories') ? 'focused error' : '' }}
+                                <label for="category">Categorias</label>
+                                <select name="categories[]" id="category" class="form-control show-tick" data-live-search="true" multiple>
+                                    @foreach($categories as $category)
+                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                {{ $errors->has('tags') ? 'focused error' : '' }}
+                                <label for="tag">Tags</label>
+                                <select name="tags[]" id="tag" class="form-control show-tick" data-live-search="true" multiple>
+                                    @foreach($tags as $tag)
+                                        <option value="{{ $tag->id }}">{{ $tag->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <a href="{{ route('admin.posts.index')}}" class="btn btn-danger m-t-15 waves-effect">VOLTAR</a>
+                            <button type="submit" class="btn btn-primary m-t-15 waves-effect">ENVIAR</button>
+                        </form>
+                    </div>
+                </div>
             </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="card">
+                    <div class="header">
+                        <h2>
+                            TEXTO
+                        </h2>
+                    </div>
+                    <div class="body">
+                        <textarea id="tinymce" name="body"></textarea>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </form>
             <!-- #END# Vertical Layout -->
 
     </div>
@@ -58,5 +100,32 @@
 @endsection
 
 @push('js')
+
+    <!-- Select Plugin Js -->
+    <script src="{{ asset('assets/backend/plugins/bootstrap-select/js/bootstrap-select.js') }}"></script>
+    <!-- TinyMCE -->
+    <script src="{{ asset('assets/backend/plugins/tinymce/tinymce.js') }}"></script>
+
+    <script>
+        $(function () {
+            //TinyMCE
+            tinymce.init({
+                selector: "textarea#tinymce",
+                theme: "modern",
+                height: 300,
+                plugins: [
+                    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                    'searchreplace wordcount visualblocks visualchars code fullscreen',
+                    'insertdatetime media nonbreaking save table contextmenu directionality',
+                    'emoticons template paste textcolor colorpicker textpattern imagetools'
+                ],
+                toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+                toolbar2: 'print preview media | forecolor backcolor emoticons',
+                image_advtab: true
+            });
+            tinymce.suffix = ".min";
+            tinyMCE.baseURL = '{{ asset('assets/backend/plugins/tinymce') }}';
+        });
+    </script>
 
 @endpush
